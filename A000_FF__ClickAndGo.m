4bf64395c14f5a300e39ed6dfb72a7af77c00e3d
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2019-06-14 13:09:05 +0200

    Fedalyzer update

--- A000_FF__ClickAndGo.m
+++ A000_FF__ClickAndGo.m
@@ -2,7 +2,7 @@ function A000_FF__ClickAndGo
 %stub to  main processing sections of program.
 tic
 %BatchrunExpArray=[1];  %'VersionTest'; 
-BatchrunExpArray=[1];  %see A000__FF_Get_JacobPathsandExperiments
+BatchrunExpArray=[2];  %see A000__FF_Get_JacobPathsandExperiments
 
 for ii=1:length(BatchrunExpArray)
     batchrunindex=BatchrunExpArray(ii);
