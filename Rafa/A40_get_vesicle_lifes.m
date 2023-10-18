d94f22f0380066f9f9bd22633f760e7f412c6afc
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2023-10-18 16:51:05 +0200

    adding radial mapping and local masking

--- Rafa/A40_get_vesicle_lifes.m
+++ Rafa/A40_get_vesicle_lifes.m
@@ -28,10 +28,13 @@ figure;
                 frame_line(fri)=fri;
                 property1(fri)=all_frames(fri).roi(roi_i).props.Area;
                 property2(fri)=all_frames(fri).roi(roi_i).props.Circularity;
-                
-                %load the image for extra analysis
+                area_pixels=all_frames(fri).roi(roi_i).props.PixelIdxList;
+                %load the roi  for extra analysis
                 im_gr=imread([init.exp_path,init.filename_green], fri);
                 im_re=imread([init.exp_path,init.filename_red], fri);
+                im_msk=0*im_gr; im_msk(area_pixels)=1;
+                
+                %user_roi masking:
                 xi=pos_xx(roi_i);
                 yi=pos_yy(roi_i);
                 Ri=pos_rr(roi_i);
@@ -40,21 +43,37 @@ figure;
                 RR=((XX-xi).^2+(YY-yi).^2).^0.5;
                 im_gr(RR>Ri)=0; 
                 im_re(RR>Ri)=0;
-                roi_gr=im_gr(loy:hiy, lox:hix);
-                roi_re=im_re(loy:hiy, lox:hix);
+                roi_gr=double(im_gr(loy:hiy, lox:hix));
+                roi_re=double(im_re(loy:hiy, lox:hix));
+                %area masking:
+                roi_msk=double(im_msk(loy:hiy, lox:hix));
+                rimwidth=15;
+                inner_roi_msk=bwmorph(roi_msk, 'erode', rimwidth);
+                band_msk= roi_msk-inner_roi_msk;
+                
+                nan_msk=band_msk;
+                nan_msk(nan_msk==0)=NaN;
+                
                 QI=TrackXY_by_QI_Init(roi_gr);
-                [~,~,~, map_gr]=TrackXY_by_QI(double(roi_gr),QI,0);
-                [~,~,~, map_re]=TrackXY_by_QI(double(roi_re),QI,0);
-
+                %track center of red:
+                [x_gre,y_gre,~, map_gr]=TrackXY_by_QI(double(roi_gr.*band_msk),QI,0);
+                [x_re,y_re,~, map_re]=TrackXY_by_QI(double(roi_re.*band_msk),QI,0);
+                %map mask green identically:
+                Xsamplinggrid=QI.X0samplinggrid+x_gre;
+                Ysamplinggrid=QI.Y0samplinggrid+y_gre;
+                map_nan=(interp2(nan_msk,Xsamplinggrid,Ysamplinggrid,'NaN'));
+                
+                figure;
+                pcolor((roi_gr/max(roi_gr(:)))+(roi_re/max(roi_re(:))));shading flat, axis equal
     
                 figure(41);
-                subplot(2,2,1), pcolor(roi_gr); shading flat, axis equal
-                subplot(2,2,2), pcolor(roi_re); shading flat, axis equal
-                subplot(2,2,3), pcolor(map_gr'); shading flat
+                subplot(2,2,1), pcolor(roi_gr.*nan_msk); shading flat, axis equal
+                subplot(2,2,2), pcolor(roi_re.*nan_msk); shading flat, axis equal
+                subplot(2,2,3), pcolor((map_gr.*map_nan)'); shading flat
                     title('vesicle map');
                     xlabel('angular position, a.u.')
                     ylabel('radial position, a.u.')
-               subplot(2,2,4), pcolor(map_re'); shading flat
+               subplot(2,2,4), pcolor((map_re.*map_nan)'); shading flat
                     title('vesicle map');
                     xlabel('angular position, a.u.')
                     ylabel('radial position, a.u.')
