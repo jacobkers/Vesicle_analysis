0573785adaf7f8a2016a7da7821610147dfc4213
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2023-12-20 14:03:19 +0100

    stub reads nd, some basic channel handling

--- python/startstub.py
+++ python/startstub.py
@@ -11,12 +11,12 @@ from scipy.ndimage import center_of_mass  # for calculation of image COM
 fig, axs = plt.subplots(1, 4)
 
 filnam="DOPC_DOPS_40 uM LUVs_1.nd2"
-coord=[[99,136],[373,326]]
+guv_centers=[[99,136],[373,326]]
 
 with nd2reader.Nd2(filnam) as images:
     for ii, chan, in enumerate(images):
         axs[ii].imshow(chan)
-        for cd in coord:
+        for cd in guv_centers:
             x=cd[0]
             y=cd[1]
             axs[ii].plot(x,y,'ro')
@@ -25,5 +25,13 @@ with nd2reader.Nd2(filnam) as images:
     fig.show()
     print("Press any key to end demo")
     input()
+
+#actions to do
+    #find center
+    #fin  rim (circle, ellipsoid) --> use radial mapper
+    #sample rim
+    #clean
+
+
 
     dum=1
