6155d25138d348639756750e0ff07f6e873b26b2
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2024-01-09 11:28:10 +0100

    start extracting rois

--- python/startstub.py
+++ python/startstub.py
@@ -3,35 +3,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nd2reader
 from pathlib import Path
-from skimage import io
-from copy import deepcopy
-from scipy.ndimage import center_of_mass  # for calculation of image COM
+
+#from my custom devlop  tools:
+import sys
+sys.path.insert(0, 'D:/jkerssemakers/Dropbox/CD_recent/BN_CD23_Jacob/analysis_general/code_development/python/')
+from how_to_do_it_examples.images import QI_tracker_tools, image_cuts
 
 
-fig, axs = plt.subplots(1, 4)
+if 1:
+    #following should go to json or so:
+    im_ori_path = str("M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/Rafa/Test Rafa_Nikon microscope/DOPC.DOPS/")
+    im_ori_path = str()
+    im_ori_name = str("DOPC_DOPS_40 uM LUVs_1.nd2")
 
-filnam="DOPC_DOPS_40 uM LUVs_1.nd2"
-guv_centers=[[99,136],[373,326]]
+source = im_ori_path + im_ori_name 
 
-with nd2reader.Nd2(filnam) as images:
-    for ii, chan, in enumerate(images):
-        axs[ii].imshow(chan)
-        for cd in guv_centers:
-            x=cd[0]
-            y=cd[1]
-            axs[ii].plot(x,y,'ro')
-            axs[ii].set_title(images.channels[ii])
+plots_out_path = Path(
+    r"D:\jkerssemakers\Dropbox\CD_Data_out\2023_Alex\2023_10_02 cluster_test\frames_1"
+)
+
+
+
+#[x y r]:
+guv_xyr=[[99,136,150 ],[373,326, 150]]
+N_guvs, dum=np.shape(guv_xyr)
+
+fig, axs = plt.subplots(N_guvs+1, 4)
+
+with nd2reader.Nd2(source) as images:
+    for ii, chan, in enumerate(images): 
+        #plot menu:
+        axs[0, ii].imshow(chan)
+        for ci, cd in enumerate(guv_xyr):
+            x0=cd[0]
+            y0=cd[1]
+            r0=cd[2]
+            roi=image_cuts.get_roi(chan,x0,y0,r0)
+            roi_array=np.array(roi)
+            var = QI_tracker_tools.QI_Tracker(roi_array)
+            axs[ci+1, ii].imshow(roi)
+            axs[0,ii].plot(x0,y0,'ro')
+            axs[0,ii].set_title(images.channels[ii])
     fig.tight_layout()
     fig.show()
     print("Press any key to end demo")
     input()
 
 #actions to do
-    #find center
+    #crop box on center estimate
+    #find center accurately
     #fin  rim (circle, ellipsoid) --> use radial mapper
     #sample rim
     #clean
-
-
 
     dum=1
