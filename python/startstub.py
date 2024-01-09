a449c55fe588e27ca790ecff3283e55306586053
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2024-01-09 13:06:57 +0100

    read rois from ImageJ csv

--- python/startstub.py
+++ python/startstub.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import nd2reader
+import csv
 from pathlib import Path
 
 #from my custom devlop  tools:
@@ -14,16 +15,45 @@ if 1:
     im_ori_path = str("M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/Rafa/Test Rafa_Nikon microscope/DOPC.DOPS/")
     im_ori_path = str()
     im_ori_name = str("DOPC_DOPS_40 uM LUVs_1.nd2")
-
+    roi_csv = str("Overlay Elements of DOPC_DOPS_40 uM LUVs_1.csv")  
 source = im_ori_path + im_ori_name 
+csv_source= im_ori_path + roi_csv
 
 plots_out_path = Path(
     r"D:\jkerssemakers\Dropbox\CD_Data_out\2023_Alex\2023_10_02 cluster_test\frames_1"
 )
 
 
+def get_roi_info(csv_source):
+    Xc=[]
+    Yc=[]
+    width=[]   
+    with open(csv_source) as f:
+        reader = csv.DictReader(f, delimiter=',')
+        for row in reader:
+            Xc.append(float(row['X']))
+            Yc.append(float(row['Y']))
+            width.append(float(row['Width']))     
+
+    XX0=(np.array(Xc)+np.array(width)/2)
+    YY0=(np.array(Yc)+np.array(width)/2)
+    RR0=(np.array(width)/2)
+    
+    return XX0, YY0,RR0
+
+
+
 #[x y r]:
-guv_xyr=[[99,136,150 ],[373,326, 150]]
+if 0:
+    guv_xyr=[[99,136,150 ],[373,326, 150]]
+else:
+    guv_xyr=[]
+    XX0, YY0,RR0 = get_roi_info(csv_source)
+    for ii, X0 in enumerate(XX0):
+        thisguv=[int(X0), int(YY0[ii]), int(RR0[ii])]
+        guv_xyr.append(thisguv)
+
+
 N_guvs, dum=np.shape(guv_xyr)
 
 fig, axs = plt.subplots(N_guvs+1, 4)
