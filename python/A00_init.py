40913358a88142854d80ed2cb86ca01dc1a91648
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2024-01-09 16:53:53 +0100

    define and run by experiment object

---
+++ python/A00_init.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+31-12-2022
+@author: jkerssemakers
+"""
+
+class Guv_experiment:
+    """
+    sets paths, names
+    """
+    def __init__(self):
+        self.mainpath_in=str("M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/Rafa/Test Rafa_Nikon microscope/DOPC.DOPS/")
+        self.mainpath_out=str("M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/Rafa/Test Rafa_Nikon microscope/DOPC.DOPS/")
+        self.subdir=str()
+        self.movienames = []
+
+# mini-archive:
+def get_exps():       
+    Exp1 = Guv_experiment()
+    Exp1.subdir=str("60 uM_1h incubation/")
+    Exp1.movienames = ["1", "2", "3", "16", "9"]
+    
+    Experiments=[Exp1]
+    
+    return Experiments
--- python/A10_guv_shell.py
+++ python/A10_guv_shell.py
@@ -10,15 +10,16 @@ import sys
 sys.path.insert(0, 'D:/jkerssemakers/Dropbox/CD_recent/BN_CD23_Jacob/analysis_general/code_development/python/')
 from how_to_do_it_examples.images import QI_tracker_tools, image_cuts
 
-#following should be more dir-based:
-mainpath_in=str("M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/Rafa/Test Rafa_Nikon microscope/DOPC.DOPS/")
-mainpath_out=str("M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/Rafa/Test Rafa_Nikon microscope/DOPC.DOPS/")
-if 0:    
-    im_ori_path = mainpath_in
-    im_ori_name = str("DOPC_DOPS_40 uM LUVs_1")
-if 1:
-    subdir=str("60 uM_1h incubation/")    
-    im_ori_names = [str("1"), str("2")]
+#local:
+from A00_init import get_exps
+
+Experiments=get_exps()
+expi=0
+mainpath_in=Experiments[expi].mainpath_in
+mainpath_out=Experiments[expi].mainpath_out
+movienames=Experiments[expi].movienames
+subdir=Experiments[expi].subdir
+
 
 im_ori_path = mainpath_in + subdir
 datapath_out= mainpath_out + subdir
@@ -26,7 +27,7 @@ testpath=Path(datapath_out)
 if not testpath.is_dir():
         testpath.mkdir()
 
-for im_ori_name in im_ori_names:
+for im_ori_name in movienames:
     source = im_ori_path + im_ori_name +str(".nd2")
     csv_source= im_ori_path + str("Overlay Elements of ") + im_ori_name +str(".csv")
 
@@ -70,7 +71,8 @@ for im_ori_name in im_ori_names:
                 roi_array=np.array(roi)
                 var = QI_tracker_tools.QI_Tracker(roi_array)
                 axs[ci+1, ii].imshow(roi)
-                axs[0,ii].plot(x0,y0,'ro')
+                #axs[0,ii].plot(x0,y0,'ro')
+
                 axs[0,ii].set_title(images.channels[ii])
         fig.tight_layout()
         fig.show()
