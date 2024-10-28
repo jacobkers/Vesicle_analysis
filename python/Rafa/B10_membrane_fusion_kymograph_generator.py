b494e8840d6f96294f3acf209890a5845cfd9d23
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2024-10-28 16:22:39 +0100

    improving plots

--- python/Rafa/B10_membrane_fusion_kymograph_generator.py
+++ python/Rafa/B10_membrane_fusion_kymograph_generator.py
@@ -8,6 +8,31 @@ import matplotlib.pyplot as plt
 import tifffile as tf
 from common_tools import guv_tools
 
+def extract_ring_values(intensity_array, center, Rmin, Rmax):
+    # Get the dimensions of the 2D array
+    rows, cols = intensity_array.shape
+    
+    # Calculate the center coordinates
+    if 0:
+        center_x, center_y = rows // 2, cols // 2
+    else:
+        center_x=center[0]
+        center_y = center[1]
+
+    # Initialize an empty list to store values within the ring
+    ring_values = []
+    
+    # Loop over each element in the 2D array
+    for x in range(rows):
+        for y in range(cols):
+            # Calculate the Euclidean distance from the center
+            distance = np.sqrt((x - center_x)**2 + (y - center_y)**2)
+            
+            # Check if the distance is within the ring boundaries
+            if Rmin <= distance <= Rmax:
+                ring_values.append(intensity_array[x, y])
+                  
+    return np.array(ring_values)
 
 def load_tiff_movie(input_path):
     """Load a TIFF movie as a list of frames."""
@@ -21,128 +46,152 @@ def load_tiff_movie(input_path):
                 break
     return frames
 
+def kymo():
+    # Example usage
+    if 0: 
+        label='2_TIRF_488_001_PCPG_Chol_small_short'
+        moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
+        savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
+        movie_filename = '2_TIRF_488_001_PCPG_Chol_small_short.tif'
+        filename ='STD_2_TIRF_488_001_PCPG_Chol_small_short.tif'
+    if 1:
+        label='2_TIRF_488_001_PCPG_Chol_long'
+        moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
+        savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
+        movie_filename = '2_TIRF_488_001_PCPG_Chol.tif'
+        filename ='STD_2_TIRF_488_001_PCPG_Chol.tif'
+    if 0:
+        moviepath= Path.cwd()
+        movie_filename = '40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1_red.tif'
+        filename = 'MAX_40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1.tif (red).tif'
 
-# Example usage
-if 0: 
-    label='2_TIRF_488_001_PCPG_Chol_small_short'
-    moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
-    savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
-    movie_filename = '2_TIRF_488_001_PCPG_Chol_small_short.tif'
-    filename ='STD_2_TIRF_488_001_PCPG_Chol_small_short.tif'
-if 1:
-    label='2_TIRF_488_001_PCPG_Chol_long'
-    moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
-    savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
-    movie_filename = '2_TIRF_488_001_PCPG_Chol.tif'
-    filename ='STD_2_TIRF_488_001_PCPG_Chol.tif'
-if 0:
-    moviepath= Path.cwd()
-    movie_filename = '40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1_red.tif'
-    filename = 'MAX_40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1.tif (red).tif'
-
-image_path =  moviepath/ filename
-movie_path =  moviepath/ movie_filename
-
-
-#load movie:
-frames=load_tiff_movie(movie_path)
-frames_array=[]
-for frame in frames:
-   frames_array.append(np.array(frame))
-frames_array=np.array(frames_array)
+    image_path =  moviepath/ filename
+    movie_path =  moviepath/ movie_filename
 
 
-#load events
-event_data_name=label +"_events" +  str(".csv")
-csv_events=savepath /  event_data_name
-csv_path_in = Path(csv_events)
-event_data = np.loadtxt(csv_events, delimiter=';')
-
-rws=[0,1]
-DT_list=[100, 20000]
-pre_shift_list=[10,200000]
- 
-DX=70 
-DY=70
-
-for event_no, event in enumerate(event_data): 
-    fig, ax=plt.subplots(4,1)   
-    for rw, DT, pre_shift in zip(rws, DT_list, pre_shift_list):   
-        x0=int(event[1])
-        y0=int(event[2])
-        t0=np.max([0, int(event[0])+int(DT/2)-pre_shift])
-        subarray, t_min, t_max, x_min, x_max, y_min, y_max=guv_tools.extract_subarray(frames_array, t0, x0, y0, DT, DX, DY)
-        #sum:
-        sumprojection_0=np.sum(subarray,axis=0)
-        sumprojection_1=np.sum(subarray,axis=1) #keeps y around Y0
-        sumprojection_2=np.sum(subarray,axis=2) #keeps x around X0
-
-        #choose full side if close to edge
-        rr,cc= np.shape(sumprojection_0)
-        if rr>cc:
-            used_proj=sumprojection_2
-            used_trace=np.sum(sumprojection_2.T,axis=0)
-            pos_min=y_min
-            pos_max=y_max
-        else:
-            used_proj=sumprojection_1
-            used_trace=np.sum(sumprojection_1.T, axis=0)
-            pos_min=x_min
-            pos_max=x_max
-        #show:
-        if rw == 0:  #zoom
-            ax[0].imshow(sumprojection_0, extent=[x_min,x_max,y_min, y_max])
-            ax[0].set_title('SUM_event' + str(event_no).zfill(4))
-            ax[3].imshow(used_proj.T, aspect='auto', extent=[t_min,t_max,pos_min,pos_max])
-            ax[3].set_ylabel("pos, pixels")
-            ax[3].set_xlabel("Time,frames")
-            t_start=t_min
-            t_stop=t_max
-        if rw == 1: #overview
-            ax[1].plot(used_trace)
-            ax[1].set_ylabel("sum intensity, a.u.")
-            ax[1].autoscale(enable=True, axis='x', tight=True)
-            ax[1].get_xaxis().set_visible(False)
-            #white lines
-            
-            ax[2].imshow(used_proj.T,aspect='auto', extent=[t_min,t_max,pos_min,pos_max,])
-            ax[2].plot([t_start,t_start],[pos_min,pos_max], 'w--',linewidth=0.5)
-            ax[2].plot([t_stop,t_stop],[pos_min,pos_max], 'w--',linewidth=0.5)
-            ax[2].set_ylabel("pos, pixels")
-
-        fig.tight_layout()
-        #final savings:
-        if rw==1:
-            #fig.tight_layout()
-            #fig.show()
-            #jpeg overview:
-            kymo_overview_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_kymo.png")
-            kymo_path= savepath/  kymo_overview_name
-            fig.savefig(kymo_path)
-        
-        if 0:
-            #tiff files:
-            kymo0_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_XY.tif")
-            kymo0_path= savepath/  kymo0_name
-            #io.imsave(savepath/  kymo0_name, sumprojection_0)
-            io.imsave(savepath / f"{kymo0_name}", sumprojection_0, check_contrast=False)
-
-            kymo1_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_XT.tif")
-            kymo1_path= savepath/  kymo0_name
-            #io.imsave(savepath/  kymo0_name, sumprojection_0)
-            io.imsave(savepath / f"{kymo1_name}", sumprojection_1, check_contrast=False)
-            dum=1
-
-            kymo2_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_YT.tif")
-            kymo2_path= savepath/  kymo0_name
-            #io.imsave(savepath/  kymo0_name, sumprojection_0)
-            io.imsave(savepath / f"{kymo2_name}", sumprojection_2, check_contrast=False)
-            dum=1
-    plt.close('All')    
+    #load movie:
+    frames=load_tiff_movie(movie_path)
+    frames_array=[]
+    for frame in frames:
+        frames_array.append(np.array(frame))
+    frames_array=np.array(frames_array)
 
 
+    #load events
+    event_data_name=label +"_events" +  str(".csv")
+    csv_events=savepath /  event_data_name
+    csv_path_in = Path(csv_events)
+    event_data = np.loadtxt(csv_events, delimiter=';')
 
+    #define a zoomed, and a full kymograph:
+    rws=[0,1]
+    DT_list=[100, 20000]
+    pre_shift_list=[10,200000]
     
+    DX=70 
+    DY=70
+
+    for event_no, event in enumerate(event_data): 
+        fig, ax=plt.subplots(4,1)   
+        for rw, DT, pre_shift in zip(rws, DT_list, pre_shift_list):   
+            x0=int(event[1])
+            y0=int(event[2])
+            t0=np.max([0, int(event[0])+int(DT/2)-pre_shift])
+            subarray, t_min, t_max, x_min, x_max, y_min, y_max=guv_tools.extract_subarray(frames_array, t0, x0, y0, DT, DX, DY)
+            
+            intensity_center=[]
+            intensity_ring1=[]
+            for sub_frame in subarray:
+                center= np.unravel_index(np.argmax(sub_frame), sub_frame.shape)
+                intensity_center.append(np.sum(extract_ring_values(sub_frame, center, 0, 5)))
+                intensity_ring1.append(np.sum(extract_ring_values(sub_frame, center, 15, 16)))
+            
+            
+            
+            #kymograph section:
+
+            #sum:
+            sumprojection_0=np.sum(subarray,axis=0)
+            sumprojection_1=np.sum(subarray,axis=1) #keeps y around Y0
+            sumprojection_2=np.sum(subarray,axis=2) #keeps x around X0
+
+            #choose full side if close to edge
+            rr,cc= np.shape(sumprojection_0)
+            if rr>cc:
+                used_proj=sumprojection_2
+                used_trace=np.sum(sumprojection_2.T,axis=0)
+                pos_min=y_min
+                pos_max=y_max
+            else:
+                used_proj=sumprojection_1
+                used_trace=np.sum(sumprojection_1.T, axis=0)
+                pos_min=x_min
+                pos_max=x_max
+            #show:
+            if rw == 0:  #zoom
+                #ax[0].imshow(sumprojection_0, extent=[x_min,x_max,y_min, y_max])
+                #ax[0].set_title('SUM_event' + str(event_no).zfill(4))
+                ax[2].imshow(used_proj.T, aspect='auto', extent=[t_min,t_max,pos_min,pos_max])
+                ax[2].set_ylabel("pos, pixels")
+                ax[2].set_xlabel("Time,frames")
+                
+                ax[3].plot(intensity_center, 'r-')
+                ax[3].plot(intensity_ring1, 'b-')
+                ax[3].legend(["center","ring"])
+                ax[3].set_ylabel("sum intensity, a.u.")
+                ax[3].autoscale(enable=True, axis='x', tight=True)
+                ax[3].get_xaxis().set_visible(False)
+
+
+                t_start=t_min
+                t_stop=t_max
+            if rw == 1: #overview
+                ax[0].plot(intensity_center, 'r-')
+                ax[0].plot(intensity_ring1, 'b-')
+                ax[0].legend(["center","ring"])
+                ax[0].set_ylabel("sum intensity, a.u.")
+                ax[0].autoscale(enable=True, axis='x', tight=True)
+                ax[0].get_xaxis().set_visible(False)
+                #white lines
+                
+                ax[1].imshow(np.log10(used_proj.T),aspect='auto', extent=[t_min,t_max,pos_min,pos_max,])
+                ax[1].plot([t_start,t_start],[pos_min,pos_max], 'w--',linewidth=0.5)
+                ax[1].plot([t_stop,t_stop],[pos_min,pos_max], 'w--',linewidth=0.5)
+                ax[1].set_ylabel("pos, pixels")
+
+            fig.tight_layout()
+            #final savings:
+            if rw==1:
+                #fig.tight_layout()
+                #fig.show()
+                #jpeg overview:
+                kymo_overview_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_kymo.png")
+                kymo_path= savepath/  kymo_overview_name
+                fig.savefig(kymo_path)
+            
+            if 0:
+                #tiff files:
+                kymo0_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_XY.tif")
+                kymo0_path= savepath/  kymo0_name
+                #io.imsave(savepath/  kymo0_name, sumprojection_0)
+                io.imsave(savepath / f"{kymo0_name}", sumprojection_0, check_contrast=False)
+
+                kymo1_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_XT.tif")
+                kymo1_path= savepath/  kymo0_name
+                #io.imsave(savepath/  kymo0_name, sumprojection_0)
+                io.imsave(savepath / f"{kymo1_name}", sumprojection_1, check_contrast=False)
+                dum=1
+
+                kymo2_name = 'kymographs_' + label +'/' + 'event' + str(event_no).zfill(4) +  str("_YT.tif")
+                kymo2_path= savepath/  kymo0_name
+                #io.imsave(savepath/  kymo0_name, sumprojection_0)
+                io.imsave(savepath / f"{kymo2_name}", sumprojection_2, check_contrast=False)
+                dum=1
+        plt.close('All')    
+
+
+
+        
 
 
 
--- python/Rafa/B20_membrane_fusion_analyzer.py
+++ python/Rafa/B20_membrane_fusion_analyzer.py
@@ -19,132 +19,182 @@ class Event:
         self.t0 = 0
         self.type = 0
 
-def travel_from_start(pre_trace, t_rise, type):
+def travel_from_start(pre_trace, t_rise, bck, direction):
     #this function analyzes a single event in more detail
     #we start at the steepest rise
-    #back in time we find the last time-above_background
-    #forward we do the same
+    #back or forward in time we find the last time-above_background
     #in both cases, times may be at be at the begin- or end-of-movie
-    #we also find the maximum intensity
-    #for hemifusion, this may be a list of 2
+       
+    t_border=t_rise
+    t_scan=t_rise
+    stopit=False
+    while t_scan>0 and t_scan<len(pre_trace)-1 and stopit == False:
+        t_scan=t_scan+direction
+        if pre_trace[t_scan]>bck:
+            t_border=t_scan
+        else:
+            stopit=True
+   
+    return t_border
+
+import numpy as np
+
+def find_steep_drops(trace, idx0, I_thresh, drop_threshold, max_drops=20):
+    """ start with a one-dimensional array of intnesity trace points with, 
+    for example a length of 225 points and a starting index idx0 pointing to an element of this array (say, idx0=14). 
+    Use the derivative of this intensity trace to find a limited number (<5) of indices idxs_down 
+    that exhibit a steep downward intensity drop, sorted by their index. 
+    These drops should be associated with local minima in the derivative trace. 
+    Include only indices from a higher index than idx0. Include only indices where the intensity 
+    is above a certain treshold level I_tresh. 
+    The data may be a bit noisy (S/N~10): please ignore local noice rises and drops. 
+     """
+
+    # Smooth the trace using a moving average to reduce noise
+    window_size = 2  # Adjustable parameter for noise smoothing
+    smoothed_trace = np.convolve(trace, np.ones(window_size)/window_size, mode='same')
     
-    #get background
-    inliers, outliers, flags=guv_tools.outlier_flag(data=pre_trace, tolerance=2, sig_change=0.7, how=1, sho=0, demo=0)
-    bck=np.median(inliers)
+    # Compute the derivative of the smoothed trace
+    derivative = np.diff(smoothed_trace)
+    #derivative = np.diff(trace)
     
-    t_dock=t_rise
-    t_scanback=t_rise
-    while t_scanback>0:
-        t_scanback=t_scanback-1
-        if pre_trace[t_scanback]>bck:
-            t_dock=t_scanback
+    deriv_noise=np.std(derivative[1:])
 
-
-    if type == 'hemifusion':
-        t_max_list=[]
-        t_min_list=[]
-    t_end_of_event
-    return t_dock
-
-# Example usage
-if 0: 
-    label='2_TIRF_488_001_PCPG_Chol_small_short'
-    moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
-    savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
-    movie_filename = '2_TIRF_488_001_PCPG_Chol_small_short.tif'
-    filename ='STD_2_TIRF_488_001_PCPG_Chol_small_short.tif'
-    xls_classification = savepath / "Events_classification_jacob_short.xlsx"  
-if 1:
-    label='2_TIRF_488_001_PCPG_Chol_long'
-    moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
-    savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
-    movie_filename = '2_TIRF_488_001_PCPG_Chol.tif'
-    filename ='STD_2_TIRF_488_001_PCPG_Chol.tif'
-    xls_classification  = savepath / "Events_classification_jacob.xlsx"  
-if 0:
-    moviepath= Path.cwd()
-    movie_filename = '40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1_red.tif'
-    filename = 'MAX_40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1.tif (red).tif'
-
-image_path =  moviepath/ filename
-movie_path =  moviepath/ movie_filename
-
-
-#load traces
-trace_data_name=label +"_traces" +  str(".csv")
-csv_traces=savepath /  trace_data_name
-csv_path_in = Path(csv_traces)
-print(csv_traces.stem)
-pre_trace_data = np.loadtxt(csv_traces, delimiter=';')
-
-#load classification file of events (contains t0,x,y,type)
-xls_source = savepath / xls_classification 
-wb = load_workbook(filename = xls_classification)
-sheet_events = wb['events']
-ColNames = {}
-Current  = 0
-for COL in sheet_events.iter_cols(1, sheet_events.max_column):
-    ColNames[COL[0].value] = Current
-    Current += 1
-event_list=[]
-for row_cells in sheet_events.iter_rows(min_row=2):
-    event=Event()
-    event.type=((row_cells[ColNames['type']].value))
-    event.x0=((row_cells[ColNames['x']].value))
-    event.y0=((row_cells[ColNames['y']].value))
-    event.t0=((row_cells[ColNames['t0']].value))
-    event_list.append(event)
-
-#we need to add:
-# dark treshold
-# end-of-trace residu
-# dock time
-# event starts (maxima)
-# release times (minima)
-# end-of-event (=eot or back-to-dark)
-
-# plot traces and start_time
-xls_source = savepath / xls_source 
-frs,N_events=np.shape(pre_trace_data)
-
-fig, ax=plt.subplots(2,2)
-for event,pre_trace in zip(event_list,pre_trace_data.T):
+    # Search for local minima in the derivative trace from idx0 onward
+    idxs_down = []
     
+    for i in range(idx0, len(derivative) - 2):  # Ensure we don't go out of bounds
+        # Check if it's a local minimum in the derivative and satisfies conditions
+        if (derivative[i] < derivative[i-1] and derivative[i] < derivative[i+1] and 
+        abs(derivative[i]-derivative[i-1])>1*deriv_noise and abs(derivative[i]-derivative[i+1])>1*deriv_noise and
+        derivative[i] < -drop_threshold and trace[i] > I_thresh):
+            idxs_down.append(i)
+            if len(idxs_down) >= max_drops:
+                break
+                
+    return idxs_down
 
-    dif_trace=np.diff(pre_trace)
-    # get typical times
-    t0=int(event.t0)
+def fusion():
+    # Example usage
+    if 0: 
+        label='2_TIRF_488_001_PCPG_Chol_small_short'
+        moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
+        savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
+        movie_filename = '2_TIRF_488_001_PCPG_Chol_small_short.tif'
+        filename ='STD_2_TIRF_488_001_PCPG_Chol_small_short.tif'
+        xls_classification = savepath / "Events_classification_jacob_short.xlsx"  
+    if 1:
+        label='2_TIRF_488_001_PCPG_Chol_long'
+        moviepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_in/2023_Rafa/2024_10_02 membrane fusion')
+        savepath=Path('M:/tnw/bn/cd\Shared/Jacob/TESTdata_out/2023_Rafa/2024_10_02 membrane fusion')
+        movie_filename = '2_TIRF_488_001_PCPG_Chol.tif'
+        filename ='STD_2_TIRF_488_001_PCPG_Chol.tif'
+        xls_classification  = savepath / "Events_classification_jacob.xlsx"  
+    if 0:
+        moviepath= Path.cwd()
+        movie_filename = '40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1_red.tif'
+        filename = 'MAX_40 uM LUVsWITHCerC6_RealTime_Series002_t000_crp-1.tif (red).tif'
 
-    #travel_from_start(pre_trace, t_rise, type)
+    image_path =  moviepath/ filename
+    movie_path =  moviepath/ movie_filename
 
-    t1=np.argmin(dif_trace)
-    tp=event.type
-    start=np.max([t0-20, 0])
-    stop=np.min([t0+100, frs])
-    trace_cut=pre_trace[start:stop]
-    dif_trace_cut=dif_trace[start:stop]
-    
-    
-    
-    #show:
-    if tp == "hemifusion":
-        ax[0,0].plot(pre_trace, '-', markersize=2)
-        ax[0,0].plot(t0,pre_trace[t0], 'ro-', markersize=4)
-        ax[0,0].set_title('traces')
-        ax[0,1].plot(dif_trace, '-', markersize=2)
-        ax[0,1].plot(t0, dif_trace[t0], 'ro-', markersize=4)
-        ax[0,1].plot(t1, dif_trace[t1], 'go-', markersize=4)
-        ax[0,1].set_title('derivative')
-        ax[1,0].plot(trace_cut, '-')
-        ax[1,0].set_title('aligned')
-        ax[1,1].plot(trace_cut/np.max(trace_cut), '-')
-        ax[1,1].set_title('normalized')
-        fig.show()
-        dum=1
-plt.close('all')
 
+    #load traces
+    trace_data_name=label +"_traces" +  str(".csv")
+    csv_traces=savepath /  trace_data_name
+    csv_path_in = Path(csv_traces)
+    print(csv_traces.stem)
+    pre_trace_data = np.loadtxt(csv_traces, delimiter=';')
+
+    #load classification file of events (contains t0,x,y,type)
+    xls_source = savepath / xls_classification 
+    wb = load_workbook(filename = xls_classification)
+    sheet_events = wb['events']
+    ColNames = {}
+    Current  = 0
+    for COL in sheet_events.iter_cols(1, sheet_events.max_column):
+        ColNames[COL[0].value] = Current
+        Current += 1
+    event_list=[]
+    for row_cells in sheet_events.iter_rows(min_row=2):
+        event=Event()
+        event.index=((row_cells[ColNames['event']].value))
+        event.type=((row_cells[ColNames['type']].value))
+        event.x0=((row_cells[ColNames['x']].value))
+        event.y0=((row_cells[ColNames['y']].value))
+        event.t0=((row_cells[ColNames['t0']].value))
+        event_list.append(event)
+
+    #we need to add:
+    # dark treshold
+    # end-of-trace residu
+    # dock time
+    # event starts (maxima)
+    # release times (minima)
+    # end-of-event (=eot or back-to-dark)
+
+    # plot traces and start_time
+    xls_source = savepath / xls_source 
+    frs,N_events=np.shape(pre_trace_data)
 
     
+    for event,pre_trace in zip(event_list,pre_trace_data.T):
+        # start of rise:
+        t_maxrise=int(event.t0)
+
+        # get background:
+        inliers, outliers, flags=guv_tools.outlier_flag(data=pre_trace[0:t_maxrise], tolerance=3, sig_change=0.7, how=1, sho=0, demo=0)
+        I_tresh=np.median(inliers)+2*np.std(inliers)
+
+
+        #first detection:
+        t_begin=travel_from_start(pre_trace, t_maxrise, I_tresh, direction=-1)
+        t_end=travel_from_start(pre_trace, t_maxrise, I_tresh, direction=1)
+        
+        # analyze differential trace
+        dif_trace=np.diff(pre_trace)
+        dif_in, dif_out, flags=guv_tools.outlier_flag(data=dif_trace, tolerance=2, sig_change=0.7, how=-1, sho=0, demo=0)
+        drop_tres=np.mean(dif_in)-4*np.std(dif_in)
+        
+        # Run the function
+        drop_indices = find_steep_drops(pre_trace,t_maxrise, I_tresh, drop_tres)
+
+
+        t1a=np.argmin(dif_trace[t_begin:t_end])
+        t1=t_begin+t1a
+        
+        tp=event.type
+        start=np.max([t_maxrise-20, 0])
+        start=0 
+        stop=np.min([t_maxrise+100, frs])
+        trace_cut=pre_trace[start:stop]
+        dif_trace_cut=dif_trace[start:stop]
+        
+        print(event.index, t_maxrise-t_begin,t_end-t_maxrise)
+        
+        #show:
+        if  1: #tp== "hemifusion":
+            fig, ax=plt.subplots(2,2)
+            ax[0,0].plot(pre_trace, '-', markersize=2)
+            ax[0,0].plot(t_maxrise,pre_trace[t_maxrise], 'ro-', markersize=8)
+            ax[0,0].plot(t_begin,pre_trace[t_begin], 'ko-', markersize=4)
+            ax[0,0].plot(t_end,pre_trace[t_end], 'mo-', markersize=4)
+            ax[0,0].set_title('traces')
+            ax[0,1].plot(dif_trace, '-', markersize=2)
+            ax[0,1].plot(t_maxrise, dif_trace[t_maxrise], 'ro-', markersize=4)
+            ax[0,1].plot(t1, dif_trace[t1], 'go-', markersize=4)
+            ax[0,1].plot(drop_indices,dif_trace[drop_indices], 'bo', markersize=4)
+            ax[0,1].set_title('derivative')
+            ax[1,0].plot(trace_cut, '-')
+            ax[1,0].set_title('aligned')
+            ax[1,1].plot(trace_cut/np.max(trace_cut), '-')
+            ax[1,1].set_title('normalized')
+            fig.show()
+            dum=1
+            plt.close('all')
+
+
+        
 
 
 
