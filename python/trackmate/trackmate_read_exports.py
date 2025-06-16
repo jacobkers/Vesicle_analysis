cdf4a445203897ab2352f35d9955ba3ecab3cb0e
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2025-06-16 13:57:08 +0200

    explore interaction traces

--- python/trackmate/trackmate_read_exports.py
+++ python/trackmate/trackmate_read_exports.py
@@ -20,6 +20,44 @@ from scipy.stats import linregress
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+from scipy.optimize import curve_fit
+
+# Define the exponential decay function
+def exp_decay(t, A, tau, C):
+    return A * np.exp(-t / tau) + C
+
+
+def fit_exponential_decay(y, n):
+    # Create x as index
+    x = np.arange(len(y))
+    
+    # Find index of maximum
+    max_index = np.argmax(y)
+
+    # Ensure there are enough points
+    if max_index + n >= len(y):
+        tau=float('nan')
+    else:
+        # Extract n points after max
+        x_fit = x[max_index + 1 : max_index + 1 + n]
+        y_fit = y[max_index + 1 : max_index + 1 + n]
+
+        # Shift x to start from 0
+        x_fit_shifted = x_fit - x_fit[0]
+
+        # Initial parameter guesses
+        A0 = y_fit[0] - y_fit[-1]
+        tau0 = n / 2
+        C0 = y_fit[-1]
+        p0 = [A0, tau0, C0]
+
+        # Fit the exponential
+        popt, _ = curve_fit(exp_decay, x_fit_shifted, y_fit, p0=p0)
+        tau=popt[1]
+        # Return the decay constant tau
+    return tau
+
+
 
 def calculate_diffusion_constant_xy(x, y, dt, max_lag=None):
     """
@@ -75,13 +113,13 @@ def calculate_diffusion_constant_xy(x, y, dt, max_lag=None):
 # plt.show()
 
 
-plot_per_file=1
+plot_per_file=0
 #read trackmate exports:
 files=[
 'PC_PG_PEG20%_50ms_TIRF_488_001_trackmate_export_3000frs',
 'PC_PG1_2ms_TIRF_488_001_trackmate_export_3000frs',
 ]
-if ~ plot_per_file: fig, ax=plt.subplots(1,1)
+if ~ plot_per_file: fig, ax=plt.subplots(1,2)
 All_labels=[]
 for filname in files:
         traces_df =pd.read_csv((filname +'.csv'),delimiter=',')
@@ -92,7 +130,11 @@ for filname in files:
         counter=0
         All_Diffusions=[]
         All_intensity_drops=[]
-        
+        All_Max_brightness=[]
+        All_decays=[]
+        All_intensity_peaks=[]
+
+
         for trace_ID in trace_IDs:    
                 Tu=traces_df["POSITION_T"][startrow:][traces_df['TRACK_ID'][startrow:]==trace_ID]
                 Xu=traces_df["POSITION_X"][startrow:][traces_df['TRACK_ID'][startrow:]==trace_ID]
@@ -123,9 +165,15 @@ for filname in files:
                         
                         D, msd, times=calculate_diffusion_constant_xy(X, Y, 1, max_lag=4)
                         All_Diffusions.append(D)
-
-                        intensity_drop=(np.max(I)-np.mean(I[-3:-1]))/np.max(I)*100
+                        Max_brighness=np.max(I)
+                        All_Max_brightness.append(Max_brighness)
+                        intensity_drop=(Max_brighness-np.mean(I[-3:-1]))/Max_brighness*100
+                        intensity_peak=(Max_brighness-I[0])/Max_brighness*100
                         All_intensity_drops.append(intensity_drop)
+                        All_intensity_peaks.append(intensity_peak)
+                        #tau_fit = fit_exponential_decay(I, 6)
+                        #All_decays.append(tau_fit)
+
         if plot_per_file:
                 ax[1,0].hist(All_Diffusions, bins=20, color='skyblue', edgecolor='k')
                 ax[1,0].set_xlabel('D, pix^2/fr')
@@ -142,11 +190,18 @@ for filname in files:
                 plt.close('all')
 
         if plot_per_file ==0:
-                ax.plot(All_Diffusions, All_intensity_drops, 'o')
-                ax.set_xlabel('D, pix^2/fr')
-                ax.set_xscale('log')
-                ax.set_ylabel('Intensity drop, %')
-                ax.legend(All_labels)
+                ax[0].plot(All_Diffusions, All_intensity_drops, 'o')
+                ax[0].set_xlabel('D, pix^2/fr')
+                ax[0].set_xscale('log')
+                ax[0].set_ylabel('Intensity drop, %')
+                ax[0].legend(All_labels)
+                
+                ax[1].plot(All_Diffusions, All_Max_brightness, 'o')
+                ax[1].set_xlabel('D, pix^2/fr')
+                ax[1].set_xscale('log')
+                ax[1].set_ylabel('peak brightness, a.u.')
+                ax[1].legend(All_labels)
+                #fig.tight_layout()
                 fig.show()
                 plt.savefig('all_data' + '_motility_vs__release.png')  # You can also use .pdf, .svg, .jpg, etc.
                 
