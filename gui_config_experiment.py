855e432ab97d5556b1e94fe24005dea18c089d29
Jacob Kerssemakers <j.w.j.kerssemakers@tudelft.nl>
2026-07-22 10:31:18 +0200

    added config

---
+++ gui_config_experiment.py
@@ -0,0 +1,6 @@
+#set paths and files here
+path_in =str("C:/Users/your_path/")
+movies_in = os.path.join(path_in, "data_overview.xlsx")
+vesicles_out = os.path.join(path_in, "vesicles_out.xlsx")
+database_file = os.path.join(path_in, "project.db")
+code_version = "v1.0-movies"
